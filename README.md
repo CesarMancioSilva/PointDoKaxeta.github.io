f939fabad6da6356f13e36275fd08e0778576e20
César Mâncio Silva <cesarmanciosilva@gmail.com>
2023-09-30 10:56:15 -0300

    ue

---
+++ README.md
@@ -0,0 +1,1 @@
+# PointDoKaxeta.github.io
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    screens:{
+      'cl':'100px',
+      "qcl":'480px',
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '1150px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
+    colors:{
+      primary:"#E1D652",
+      bgblack:"#121212",
+      secondary:"#332F2E",
+      bgmenu:"#474443",
+      white:'#fff',
+      red:"red"
+    }, fontFamily:{
+      body:['Roboto']
+  },
+    extend: {},
+  },
+  plugins: [],
+}
+
